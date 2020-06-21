3ee003d061d2e1fc116c043dbba6605a6f5e9f94
Kevin Helma <helmakevin@gmail.com>
2020-06-21 12:53:27 +0700

    added handle and stop

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+)
 
 type Task struct {
 	closed chan struct{}
@@ -16,5 +20,41 @@ func (t *Task) Run() {
 			handle()
 			os.Exit(0)
 		}
+	}
+}
+
+func (t *Task) Stop() {
+	close(t.closed)
+	t.wg.Wait()
+}
+
+func handle() {
+	// # Read integer
+	var i int
+	fmt.Print("Input jumlah loket: ")
+	fmt.Scanf("%d", &i)
+
+	if i < 1 || i > 5 {
+		fmt.Println("Loket harus antara 1-5")
+		os.Exit(0)
+	}
+
+	//queque in second
+	queque := []int{1, 2, 4, 2, 3, 5, 2, 3, 1, 3}
+	numJobs := len(queque)
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= i; w++ {
+		go worker(queque, w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		<-results
 	}
 }
