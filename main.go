cb740b1c9557662f227aafbb5b401f325b9d4770
Kevin Helma <helmakevin@gmail.com>
2020-06-21 12:52:57 +0700

    added Task and Run

--- main.go
+++ main.go
@@ -1,2 +1,20 @@
 package main
 
+import "sync"
+
+type Task struct {
+	closed chan struct{}
+	wg     sync.WaitGroup
+}
+
+func (t *Task) Run() {
+	for {
+		select {
+		case <-t.closed:
+			return
+		default:
+			handle()
+			os.Exit(0)
+		}
+	}
+}
